b94259d4deec6d87fc2125d50aa885ac350d95e3
Тимофей Нецветаев <timofei.netsvetaev@digital-ecosystems.ru>
2019-07-22 23:33:53 +0700

    Начало работы над системой  UI. Верстка нового UI

---
+++ Assets/Scripts/UI/Elements/Popups/Popup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Popup : MonoBehaviour
+{
+	[HideInInspector]
+	public UnityEvent onClose;
+	[HideInInspector]
+	public UnityEvent onOpen;
+
+	private void Awake()
+	{
+		if (onClose == null)
+		{
+			onClose = new UnityEvent();
+		}
+
+		if (onOpen == null)
+		{
+			onOpen = new UnityEvent();
+		}
+	}
+
+	public void Close()
+	{
+		Time.timeScale = gameObject.activeSelf ? 0 : 1;
+		onClose.Invoke();
+	}
+
+	public void Open()
+	{
+		Time.timeScale = gameObject.activeSelf ? 0 : 1;
+		onOpen.Invoke();
+	}
+}
---
+++ Assets/Scripts/UI/Elements/UIController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+	public GameObject pausePopup;
+
+	public void ShowPause()
+	{
+		pausePopup.SetActive(!pausePopup.activeSelf);
+		Time.timeScale = pausePopup.activeSelf ? 0 : 1;
+		Debug.Log(Time.timeScale);
+	}
+}
